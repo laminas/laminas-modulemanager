9880b3d89c77fb8fd5da53ee20fc84bc4ef9723f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:25:38 -0600

    Rewriting as Laminas Project package

--- test/Listener/ModuleDependencyCheckerListenerTest.php
+++ test/Listener/ModuleDependencyCheckerListenerTest.php
@@ -1,33 +1,31 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_ModuleManager
+ * @see       https://github.com/laminas/laminas-modulemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-modulemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-modulemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\ModuleManager\Listener;
+namespace LaminasTest\ModuleManager\Listener;
 
+use Laminas\ModuleManager\Listener\ModuleDependencyCheckerListener;
+use Laminas\ModuleManager\ModuleManager;
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\ModuleManager\Listener\ModuleDependencyCheckerListener;
-use Zend\ModuleManager\ModuleManager;
 
 class ModuleDependencyCheckerListenerTest extends TestCase
 {
     /**
-     * @covers \Zend\ModuleManager\Listener\ModuleDependencyCheckerListener::__invoke
+     * @covers \Laminas\ModuleManager\Listener\ModuleDependencyCheckerListener::__invoke
      */
     public function testCallsGetModuleDependenciesOnModuleImplementingInterface()
     {
         //$moduleManager = new ModuleManager(array());
         //$moduleManager->getEventManager()->attach('loadModule', new ModuleDependencyCheckerListener(), 2000);
 
-        $module = $this->getMock('Zend\ModuleManager\Feature\DependencyIndicatorInterface');
+        $module = $this->getMock('Laminas\ModuleManager\Feature\DependencyIndicatorInterface');
         $module->expects($this->once())->method('getModuleDependencies')->will($this->returnValue(array()));
 
-        $event = $this->getMock('Zend\ModuleManager\ModuleEvent');
+        $event = $this->getMock('Laminas\ModuleManager\ModuleEvent');
         $event->expects($this->any())->method('getModule')->will($this->returnValue($module));
 
         $listener = new ModuleDependencyCheckerListener();
@@ -35,14 +33,14 @@ class ModuleDependencyCheckerListenerTest extends TestCase
     }
 
     /**
-     * @covers \Zend\ModuleManager\Listener\ModuleDependencyCheckerListener::__invoke
+     * @covers \Laminas\ModuleManager\Listener\ModuleDependencyCheckerListener::__invoke
      */
     public function testCallsGetModuleDependenciesOnModuleNotImplementingInterface()
     {
         $module = $this->getMock('stdClass', array('getModuleDependencies'));
         $module->expects($this->once())->method('getModuleDependencies')->will($this->returnValue(array()));
 
-        $event = $this->getMock('Zend\ModuleManager\ModuleEvent');
+        $event = $this->getMock('Laminas\ModuleManager\ModuleEvent');
         $event->expects($this->any())->method('getModule')->will($this->returnValue($module));
 
         $listener = new ModuleDependencyCheckerListener();
@@ -50,18 +48,18 @@ class ModuleDependencyCheckerListenerTest extends TestCase
     }
 
     /**
-     * @covers \Zend\ModuleManager\Listener\ModuleDependencyCheckerListener::__invoke
+     * @covers \Laminas\ModuleManager\Listener\ModuleDependencyCheckerListener::__invoke
      */
     public function testNotFulfilledDependencyThrowsException()
     {
         $module = $this->getMock('stdClass', array('getModuleDependencies'));
         $module->expects($this->once())->method('getModuleDependencies')->will($this->returnValue(array('OtherModule')));
 
-        $event = $this->getMock('Zend\ModuleManager\ModuleEvent');
+        $event = $this->getMock('Laminas\ModuleManager\ModuleEvent');
         $event->expects($this->any())->method('getModule')->will($this->returnValue($module));
 
         $listener = new ModuleDependencyCheckerListener();
-        $this->setExpectedException('Zend\ModuleManager\Exception\MissingDependencyModuleException');
+        $this->setExpectedException('Laminas\ModuleManager\Exception\MissingDependencyModuleException');
         $listener->__invoke($event);
     }
 }
