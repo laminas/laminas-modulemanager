974a08524c04efd4e78e749a287417eb5d87e206
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:26:03 -0600

    Rewriting as Laminas Project package

--- src/Listener/DefaultListenerAggregate.php
+++ src/Listener/DefaultListenerAggregate.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-modulemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-modulemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-modulemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\ModuleManager\Listener;
+namespace Laminas\ModuleManager\Listener;
 
-use Zend\EventManager\EventManagerInterface;
-use Zend\EventManager\ListenerAggregateInterface;
-use Zend\ModuleManager\ModuleEvent;
-use Zend\Stdlib\CallbackHandler;
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\EventManager\ListenerAggregateInterface;
+use Laminas\ModuleManager\ModuleEvent;
+use Laminas\Stdlib\CallbackHandler;
 
 /**
  * Default listener aggregate
