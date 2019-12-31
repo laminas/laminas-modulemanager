ad466c20e5dce29afc47fa8f9daae540ac87f107
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:25:51 -0600

    Rewriting as Laminas Project package

--- src/Listener/AutoloaderListener.php
+++ src/Listener/AutoloaderListener.php
@@ -1,17 +1,16 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-modulemanager for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-modulemanager/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-modulemanager/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\ModuleManager\Listener;
+namespace Laminas\ModuleManager\Listener;
 
-use Zend\Loader\AutoloaderFactory;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\ModuleEvent;
+use Laminas\Loader\AutoloaderFactory;
+use Laminas\ModuleManager\Feature\AutoloaderProviderInterface;
+use Laminas\ModuleManager\ModuleEvent;
 
 /**
  * Autoloader listener
