c1223c7f07e9917409cf67b45f6d25036fa74575
Michał Bundyra <contact@webimpress.com>
2020-08-25 11:00:06 +0100

    Update new exception class
    - add private constructor
    - add RTH in method
    - update PHPDocs
    - mark method as internal
    
    Signed-off-by: Michał Bundyra <contact@webimpress.com>

--- src/Listener/Exception/ConfigCannotBeCachedException.php
+++ src/Listener/Exception/ConfigCannotBeCachedException.php
@@ -10,17 +10,23 @@ namespace Laminas\ModuleManager\Listener\Exception;
 
 use Brick\VarExporter\ExportException;
 use RuntimeException;
+use Throwable;
 
 use function sprintf;
 
 final class ConfigCannotBeCachedException extends RuntimeException
 {
+    private function __construct($message = "", $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
     /**
-     * @param ExportException $exportException
+     * @internal
      *
-     * @return ConfigCannotBeCachedException
+     * @return self
      */
-    public static function fromExporterException(ExportException $exportException)
+    public static function fromExporterException(ExportException $exportException): self
     {
         return new self(
             sprintf(
